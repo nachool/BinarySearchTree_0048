a9be1b60837e4085807114bb6053fedb66ea7602
nachool <nachma.olivia1394@gmail.com>
2024-06-13 18:46:29 +0700

    membuat prosedur search

--- BinarySearchTree_0048/BinarySearchTree_0048.cpp
+++ BinarySearchTree_0048/BinarySearchTree_0048.cpp
@@ -50,8 +50,17 @@ class BinaryTree
         } 
         else if (element > parent->info) // If the value in the data field of the new node is greater than that of the
         {
+            parent->rightchild = newNode; // Make the right child of the parent point to the new node
+        } 
+    }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
 
     }
+
 };
+
+
 
 
