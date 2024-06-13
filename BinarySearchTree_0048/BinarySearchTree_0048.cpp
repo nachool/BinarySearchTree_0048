131141950827baba82cb231bcf14a81bca2f3ef2
nachool <nachma.olivia1394@gmail.com>
2024-06-13 18:35:54 +0700

    inserting algorithm

--- BinarySearchTree_0048/BinarySearchTree_0048.cpp
+++ BinarySearchTree_0048/BinarySearchTree_0048.cpp
@@ -30,7 +30,21 @@ class BinaryTree
     void insert(string element) // Insert a node in the binary search tree
     {
         Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
-        
+        newNode->info = element; //assign value to the data field of the new node 
+        newNode->leftchild = nullptr; // Make the left child of the new node point to the NULL
+        newNode->rightchild = nullptr;  // Make the right child of the new node point to the NULL
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // If the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // Mark the new node as ROOT
+            return; // Exit
+        }
+
+    }
 };
 
 
